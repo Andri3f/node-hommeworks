f9f4c993c088b140c380b6cc4c1cb86e565d3da0
Andri Kocherha <kotschergaandrij@gmail.com>
2024-10-21 14:56:10 +0300

    done homework 4

---
+++ homework4/fishing-shop/controllers/mainController.mjs
@@ -0,0 +1,6 @@
+class MainController {
+	static renderHome(req, res) {
+		res.render("index", {})
+	}
+}
+export default MainController
---
+++ homework4/fishing-shop/controllers/productsController.mjs
@@ -0,0 +1,26 @@
+import productsModule from "../models/productsModel.mjs"
+class ProductsController {
+	static productsAllPage(req, res) {
+		const products = productsModule.getAllProducts()
+		res.render("productsViews/productsView", { products })
+	}
+	static specificProduct(req, res) {
+		const id = req.params.id
+		const product = productsModule.getProdById(id)
+		res.render("productsViews/specificProdView", { product })
+	}
+	static newProduct(req, res) {
+		res.render("productsViews/newProductView")
+	}
+	static createNewProd(req, res) {
+		const prodData = req.body
+		productsModule.addProduct(prodData)
+		res.redirect("/products")
+	}
+	static deleteProd(req, res) {
+		const id = req.params.id
+		productsModule.deleteProdById(id)
+		res.redirect("/products")
+	}
+}
+export default ProductsController
